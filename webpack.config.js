5ef2744d80b305f58da9a750c12d180a874a0f5c
Ivonne <ivonnebenites2015@gmail.com>
2023-09-17 20:38:28 -0500

    Update webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
   },
   devtool: 'inline-source-map',
   devServer: {
